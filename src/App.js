1221f545b8498e608cc48919692065012c63219b
Maayanalt <maayanalt@gmail.com>
2022-07-28 22:52:11 +0300

    created product details component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   faCartShopping,
   faHeart,
 } from "@fortawesome/free-solid-svg-icons";
-// import ProductDetails from "./productDetails/ProductDetails.js";
+// import ProductDetails from "./components/ProductDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { InputGroup, Button, Form } from "react-bootstrap";
 // import Login from "./Login/Login";

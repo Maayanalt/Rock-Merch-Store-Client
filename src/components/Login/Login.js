579cac3af110885ed744b0da4438bb48ea8ef2a6
Maayanalt <maayanalt@gmail.com>
2022-07-29 15:47:58 +0300

    created login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,24 @@
+import inputTypes from "../DAL/dbForm";
+import FormTemplate from "../Form/FormTemplate";
+import { Alert, Container } from "react-bootstrap";
+import "./Login.css";
+
+function Login() {
+  return (
+    <Container className="px-4 mb-5">
+      <Alert key="1" variant="secondary" className="w-50">
+        <Alert.Heading as="h2">Login</Alert.Heading>
+        <hr />
+        <FormTemplate
+          inputs={{
+            username: inputTypes.username,
+            password: inputTypes.password,
+          }}
+          submit="sign in"
+        ></FormTemplate>
+      </Alert>
+    </Container>
+  );
+}
+
+export default Login;

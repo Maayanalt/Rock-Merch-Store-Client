bc0e33bc62eff5664fd98e0d61efa39bc459d9f0
Maayanalt <maayanalt@gmail.com>
2022-08-18 21:49:45 +0300

    added more styles and the navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHouse,
-  faCircleInfo,
-  faEnvelope,
-  faUser,
-  faCartShopping,
-  faHeart,
-} from "@fortawesome/free-solid-svg-icons";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
+import NavbarComp from "./components/Navbar/NavbarComp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { InputGroup, Button, Form } from "react-bootstrap";
 import Login from "./components/Login/Login";
@@ -18,7 +10,7 @@ function App() {
     <div>
       <header className="d-flex justify-content-between align-items-center px-4">
         <img src="/logo.png" id="logo" alt="logo" />
-        <InputGroup className="w-25">
+        <InputGroup id="search">
           <Form.Control
             placeholder="Search"
             aria-label="Search"
@@ -33,44 +25,7 @@ function App() {
           </Button>
         </InputGroup>
       </header>
-      <nav id="navbar" className="navbar navbar-expand-sm navbar-dark mb-4">
-        <div className="container-fluid">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon> Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faCircleInfo}></FontAwesomeIcon> About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon> Contact us
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="/">
-                <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <NavbarComp></NavbarComp>
       <main></main>
       <footer>
         <p>©2022 Built by Maayan Altarac</p>

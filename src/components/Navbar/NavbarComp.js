6008016265c283096b014bcc594b3f62a2b78ab2
Maayanalt <maayanalt@gmail.com>
2022-08-18 15:36:01 +0300

    added the navbar component

---
+++ src/components/Navbar/NavbarComp.js
@@ -0,0 +1,88 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faCircleInfo,
+  faEnvelope,
+  faUser,
+  faCartShopping,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import "./NavbarComp.css";
+
+function NavbarComp() {
+  return (
+    <Navbar id="navbar" expand="sm" className="navbar-dark mb-4">
+      <Container fluid>
+        <Navbar.Toggle
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse className="justify-content-between" id="navbarToggler">
+          <Nav>
+            <Nav.Link className="active" href="#">
+              <FontAwesomeIcon
+                icon={faHouse}
+                className="pe-2 pe-sm-2"
+              ></FontAwesomeIcon>
+              Home
+            </Nav.Link>
+            <Nav.Link className="active" href="#">
+              <FontAwesomeIcon
+                icon={faCircleInfo}
+                className="pe-2 pe-sm-2"
+              ></FontAwesomeIcon>
+              About
+            </Nav.Link>
+            <Nav.Link className="active" href="#">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="pe-2 pe-sm-2"
+              ></FontAwesomeIcon>
+              Contact us
+            </Nav.Link>
+            <Nav.Link className="active d-inline-block d-sm-none" href="#">
+              <FontAwesomeIcon
+                icon={faUser}
+                className="pe-2 pe-sm-0"
+              ></FontAwesomeIcon>
+              My Account
+            </Nav.Link>
+            <Nav.Link className="active d-inline-block d-sm-none" href="#">
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                className="pe-2 pe-sm-0"
+              ></FontAwesomeIcon>
+              My cart
+            </Nav.Link>
+            <Nav.Link className="active d-inline-block d-sm-none" href="#">
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="pe-2 pe-sm-0"
+              ></FontAwesomeIcon>
+              My Wishlist
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Nav className="d-none d-sm-flex">
+          <Nav.Link className="active" href="#">
+            <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+          </Nav.Link>
+          <Nav.Link className="active" href="#">
+            <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+          </Nav.Link>
+          <Nav.Link className="active" href="#">
+            <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;

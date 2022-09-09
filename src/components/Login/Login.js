3db14dcd2e4cec3fde8e2f7da0e6e1c4d54ce3cf
Maayanalt <maayanalt@gmail.com>
2022-09-09 21:52:12 +0300

    created a submit function to validate values with the server

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import inputTypes from "../../DAL/formData";
 import FormTemplate from "../Form/FormTemplate";
 import { Alert, Container } from "react-bootstrap";
+import { postLogin } from "../../DAL/api";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  async function validateUserInServer(formData) {
+    const valid = await postLogin(
+      formData.email.value,
+      formData.password.value
+    );
+    if (valid) navigate("/");
+    else alert("wrong password or email, try again !");
+  }
+
   return (
     <Container className="px-4 mb-5">
       <Alert key="1" variant="secondary" className="w-50">
@@ -11,10 +24,11 @@ function Login() {
         <hr />
         <FormTemplate
           inputs={{
-            username: inputTypes.username,
+            email: inputTypes.email,
             password: inputTypes.password,
           }}
           submit="sign in"
+          postToServer={validateUserInServer}
         ></FormTemplate>
       </Alert>
     </Container>

498e27e02e2f669b2964ba04fbf9ba8ef4608a63
Maayanalt <maayanalt@gmail.com>
2022-11-12 18:16:37 +0200

    created my account and overview components

---
+++ src/components/MyAccount/Overview.js
@@ -0,0 +1,32 @@
+import { TbHandRock } from "react-icons/tb";
+import { Button } from "react-bootstrap";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { postLogout } from "../../DAL/api";
+
+function Overview() {
+  const navigate = useNavigate();
+  const user = useOutletContext();
+
+  async function logOut() {
+    const status = await postLogout();
+    if (status) navigate("/");
+  }
+
+  return (
+    <div>
+      <h2>
+        Hello, {user.firstName} {user.lastName} <TbHandRock></TbHandRock>
+      </h2>
+      <Button
+        variant="outline-info"
+        className="shadow-none px-2 py-1 col-lg-2 mt-4"
+        id="log-out"
+        onClick={logOut}
+      >
+        Log out
+      </Button>
+    </div>
+  );
+}
+
+export default Overview;

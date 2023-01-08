bc3e3e54c6bd2dab8ba1085eaf700a45147689fc
Maayanalt <maayanalt@gmail.com>
2023-01-08 19:51:31 +0200

    changed some styles

--- src/components/HomePage/ProductCard.js
+++ src/components/HomePage/ProductCard.js
@@ -69,8 +69,9 @@ function ProductCard({ images, name, sizes, price, description, productID }) {
               description,
               price,
             }}
+            className="product-title"
           >
-            <Card.Title className="card-title">{name}</Card.Title>
+            <Card.Title>{name}</Card.Title>
           </Link>
           <Card.Text className="card-text">${price}</Card.Text>
           <Row className="d-flex gap-1 justify-content-center">
